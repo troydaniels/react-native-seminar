53f13eaf3ad064d885823085b48ed3d997e7741a
Jonathan Yeung <jonathanyeung1997@gmail.com>
2019-09-26 14:16:28 +1000

    init files

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+class App extends React.Component{
+  static navigationOptions = ({ navigation }) => {
+    const headerLeft = <Header/>;
+  };
+
+  render(){
+    return(
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
