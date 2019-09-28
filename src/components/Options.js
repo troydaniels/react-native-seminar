ef47debe1cb4b2a74a9ce33feed94737e79f19b2
stefanvan94 <48899966+stefanvan94@users.noreply.github.com>
2019-09-28 10:26:02 +1000

    Answer verification (#6)
    * Verifies answer
    
    * Linting changes
    
    * Linting changes
    
    * Gameover functionality
    
    * Linter change
    
    * Fixes to display correct answer

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,21 +1,37 @@
 import React from "react";
 import { observer } from "mobx-react/dist/mobx-react";
-import { View, Button } from "react-native";
+import PropTypes from "prop-types";
+import { View, Button, Text } from "react-native";
 import appStore from "../stores/AppStore";
 
 @observer
 class Options extends React.Component {
   render() {
+    const { gameOver } = appStore;
     const { options } = appStore.question;
-    return (
+    const { verifyAnswer } = this.props;
+
+    const handleOnPress = option => {
+      verifyAnswer(option);
+    };
+
+    return !gameOver ? (
       <View>
-        <Button title={options[0]} />
-        <Button title={options[1]} />
-        <Button title={options[2]} />
-        <Button title={options[3]} />
+        <Button title={options[0]} onPress={() => handleOnPress(options[0])} />
+        <Button title={options[1]} onPress={() => handleOnPress(options[1])} />
+        <Button title={options[2]} onPress={() => handleOnPress(options[2])} />
+        <Button title={options[3]} onPress={() => handleOnPress(options[3])} />
+      </View>
+    ) : (
+      <View>
+        <Text>GAME OVER!</Text>
       </View>
     );
   }
 }
+
+Options.propTypes = {
+  verifyAnswer: PropTypes.func.isRequired
+};
 
 export default Options;
--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,48 +1,93 @@
-import { configure, observable, action } from 'mobx';
-import axios from 'axios';
+import { configure, observable, action } from "mobx";
+import axios from "axios";
 
-configure({ enforceActions: 'always' });
+configure({ enforceActions: "always" });
 
 class AppStore {
-    @observable score = 0;
+  @observable score = 0;
 
-    @observable lives = 3;
+  @observable lives = 3;
 
-    @observable question = undefined;
+  @observable question = undefined;
 
-    @action
-    resetStore = () => {
-        this.score = 0;
-        this.lives = 0;
-        this.question = undefined;
+  @observable result = undefined;
+
+  @observable gameOver = false;
+
+  @action
+  resetStore = () => {
+    this.score = 0;
+    this.lives = 0;
+    this.question = undefined;
+  };
+
+  @action
+  resetResult = () => {
+    this.result = undefined;
+  };
+
+  @action
+  decrementLives = () => {
+    this.lives -= 1;
+  };
+
+  @action
+  incrementScore = () => {
+    this.score += 1;
+  };
+
+  @action
+  getNewQuestion = () => {
+    this.resetResult();
+
+    axios
+      .get("http://localhost:8080/get_question")
+      .then(response => this.setQuestion(response.data))
+      .catch(error => {
+        console.error(
+          `There has been a problem with your fetch operation: ${error.message}`
+        );
+      });
+  };
+
+  @action
+  setQuestion = question => {
+    this.question = question;
+  };
+
+  @action
+  verifyAnswer = option => {
+    const { uri } = this.question;
+    const postData = {
+      uri,
+      selection: `${option}.svg`
     };
 
-    @action
-    decrementLives = () => {
-        this.lives -= 1;
-    };
+    axios
+      .post("http://localhost:8080/verify_answer", postData)
+      .then(response => this.setResult(response.data.correct))
+      .catch(error => {
+        console.error(
+          `There has been a problem with your fetch operation: ${error.message}`
+        );
+      });
+  };
 
-    @action
-    incrementScore = () => {
-        this.score += 1;
-    };
+  @action
+  setResult = correct => {
+    if (correct) {
+      this.result = true;
+      this.incrementScore();
+    } else {
+      this.result = false;
+      this.decrementLives();
+    }
+  };
 
-    @action
-    getNewQuestion = () => {
-        axios
-            .get('http://localhost:8080/get_question')
-            .then(response => this.setQuestion(response.data))
-            .catch(error => {
-                console.error(
-                    `There has been a problem with your fetch operation: ${error.message}`,
-                );
-            });
-    };
-
-    @action
-    setQuestion = question => {
-        this.question = question;
-    };
+  @action
+  setGameOver = _ => {
+    this.gameOver = true;
+  };
 }
 
 const appStore = new AppStore();
